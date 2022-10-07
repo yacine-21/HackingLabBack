516dbfafd978cd0574c2a68aa96f1356c6cbb440
yacine-21 <yacinelyoubi37@gmail.com>
2022-10-07 10:59:30 +0200

    new endpoint

--- src/api.js
+++ src/api.js
@@ -16,7 +16,7 @@ cors({
 app.use("/.netlify/functions/api", router); // path must route to lambda
 
 router.get("/", (req, res) => {
-	res.json("hello world");
+	res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
 router.get("/test", (req, res) => {
