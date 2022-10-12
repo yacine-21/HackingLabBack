6d6c026c4f74cd6be5d1b94a5c14b62a3a138344
yacine-21 <yacinelyoubi37@gmail.com>
2022-10-12 17:09:27 +0200

    add build command

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ const app = express();
 const router = express.Router();
 const cors = require("cors");
 
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
